98a9fab76d66df575c4473b4704cb4ff36446d2b
AxelLGM <oaxelmarinho@gmail.com>
2025-07-31 12:56:22 -0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+baseTriangulo = float(input("digite o valor da base do triangulo: "))
+alturaTriangulo = float(input("digite o valor da altura do triangulo: "))
+
+areaTriangulo = (baseTriangulo*alturaTriangulo)
+
+print(f"a area do triangulo é {areaTriangulo:.2f}")
+
+-----------------------------------------------
+
+baseTriangulo = float(input("Digite o valor da base do triangulo: "))
+alturaTriangulo = float(input("Digite o valor da altura do triangulo: "))
+
+areaTriangulo = (baseTriangulo * alturaTriangulo) / 2
+
+print(f"A área do triangulo é {areaTriangulo:.2f}") 
+
+-----------------------------------------------
